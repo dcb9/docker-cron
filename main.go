5a72000e0842f6a8b1fc085fc9d564d3a4674bb0
dcb9 <bob@phpor.me>
2020-01-25 15:56:28 +0800

    Init

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"github.com/robfig/cron"
+	log "github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var (
+	filePath string
+	job      string
+)
+
+func init() {
+	log.SetLevel(log.InfoLevel)
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+
+	flag.StringVar(&filePath, "file", "/etc/crontab", "crontab file path")
+	flag.StringVar(&job, "job", "", "cron job: * * * * * ls")
+}
+
+func main() {
+	flag.Parse()
+
+	c := myCron{
+		Cron: cron.New(),
+	}
+	if job != "" {
+		c.AddJob(job)
+	} else {
+		c.AddFromFile(filePath)
+	}
+
+	c.Run()
+}
+
+var lineExp = regexp.MustCompile(`^(\S+\s+\S+\s+\S+\s+\S+\s+\S+)\s+(.+)$`)
+
+type myCron struct {
+	*cron.Cron
+}
+
+func (c *myCron) AddJob(job string) {
+	m := lineExp.FindStringSubmatch(job)
+	c.AddFunc(m[1], func() {
+		out, err := exec.Command("bash", "-c", m[2]).CombinedOutput()
+		if err != nil {
+			log.Printf("command: %s out: \r\n%s with error: %s", m[2], out, err.Error())
+		} else {
+			log.Printf("command: %s out: \r\n%s", m[2], out)
+		}
+	})
+}
+
+func (c *myCron) AddFromFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("open file %s error: %s", path, err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		if lineExp.MatchString(line) == true {
+			c.AddJob(line)
+		}
+	}
+}
+
+func (c *myCron) Run() {
+	if len(c.Entries()) == 0 {
+		log.Fatal("exited: no job found")
+	}
+
+	c.Cron.Run()
+}
+
